161257ff232b708dc9573f668c94d6a101adc7fc
hanbit10 <chanbit10@gmail.com>
2024-06-26 10:17:21 +0200

    update card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
-
+  pickCardAnimation = false;
+  constructor() {}
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
