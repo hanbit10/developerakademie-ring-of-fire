65636b6a8037410f1d75697698337ff6137914ca
hanbit10 <chanbit10@gmail.com>
2024-07-04 16:34:55 +0200

    update

--- src/app/edit-player/edit-player.component.ts
+++ src/app/edit-player/edit-player.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-edit-player',
   standalone: true,
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './edit-player.component.html',
   styleUrl: './edit-player.component.scss',
 })
